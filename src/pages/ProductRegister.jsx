ed65c79e3ada9da22836255e5fc2c598321a2085
DESKTOP-063TBPA\TJ <gbg9609@naver.com>
2023-11-15 18:12:52 +0900

    useState 리팩토링 / textarea 미완

--- src/pages/ProductRegister.jsx
+++ src/pages/ProductRegister.jsx
@@ -5,42 +5,47 @@ import ProductCategory from '../component/Register/ProductCategory';
 export default function ProductRegister() {
 
   // 초기 위치 정보 (나중에 API 데이터로 변경될 수 있음)
-  let location = '경기도 성남시 중원구 성남동' 
+  let location = '경기도 성남시 중원구 성남동'
   // 상태 관리를 위한 useState 훅 사용
   let [form, setForm] = useState({ 'productName': '', 'category': '', location, 'price': '' })
   let [textNum, setTextNum] = useState(0);  // 상품이름 input 글자수 체크
-  let [show, setShow] = useState(false);  // 상품명을 입력 멘트온오프
   const [active, setActive] = useState(false) // '상세 카테고리를 선택해주세요.' 멘트 온오프
   let [first, setFirst] = useState(''); // 카테고리 천째칸
   let [second, setScond] = useState(''); // 카테고리 둘째칸
   let [last, setLast] = useState(''); // 카테고리 셋째칸
   let checkNum = /^[0-9]/; // 가격 숫자만 정규식
-  let [priceNotice, setPriceNotice] = useState(false)
+  let [deliverPrice, setDeliverPrice] = useState(false) // 배달비칸 온 오프
+  let [notice, setNotice] = useState([false, false, false, false]) // input 주의사항 메세지
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setForm({ ...form, [name]: value });
   } //input 값 onChange시 value값 재 할당
-
-  let textNumCheck = (e) => {
+  const copyArr = (boolean, n) => {
+    let copy = [...notice]
+    copy[n] = boolean
+    setNotice(copy)
+  }
+  let textNumCheck = (e, n) => {
     let txtNum = e.target.value.length;
     setTextNum(txtNum); //txtNum의 수에따라 input 테두리색을 위해 클래스 add , remove
     if (txtNum === 1) {
-      setShow(true)
+
       e.target.classList.add('on')
+      copyArr(true, n)
     } else {
-      setShow(false)
+      copyArr(false, n)
       e.target.classList.remove('on')
     }
   }
-  let priceNumCheck = (e) => {
+  let priceNumCheck = (e, n) => {
     let priceNum = e.target.value
     if (parseInt(priceNum) < 100) {
       e.target.classList.add('on')
-      setPriceNotice(true)
+      copyArr(true, n)
     } else {
       e.target.classList.remove('on')
-      setPriceNotice(false)
+      copyArr(false, n)
     }
     if (!checkNum.test(priceNum)) {
       setForm({ ...form, 'price': '' });
@@ -48,6 +53,24 @@ export default function ProductRegister() {
 
     setForm({ ...form, 'price': priceNum !== '' ? parseInt(priceNum) : '' });
   }
+
+  let deliverPriceCheck = (e, n) => {
+    let deliverPrice = e.target.value
+    if (parseInt(deliverPrice) < 100) {
+      copyArr(true, n)
+      e.target.classList.add('on')
+    } else if (parseInt(deliverPrice) > 100000) {
+      copyArr(true, n)
+      e.target.classList.add('on')
+      e.target.value = 100000
+      alert('배송비는 100원부터 100,000원까지 입력할 수 있어요.')
+    } else {
+      e.target.classList.remove('on')
+      copyArr(false, n)
+    }
+
+  }
+
 
   let catagoryClick = (boolean, value, txt, n) => {
     setActive(boolean) // active boolean 값에 따라  '상세 카테고리를 선택해주세요.' 텍스트 노출 미노출
@@ -71,6 +94,18 @@ export default function ProductRegister() {
     console.log(form);
   }
 
+  let textAreaCheck = (e, n) => {
+    let txtNum = e.target.value.length;
+    if (txtNum < 10) {
+
+      e.target.classList.add('on')
+      copyArr(true, n)
+    } else {
+      copyArr(false, n)
+      e.target.classList.remove('on')
+    }
+  }
+
   return (
     <form className="inner" onSubmit={handleSubmit}>
 
@@ -90,12 +125,12 @@ export default function ProductRegister() {
           <p className="inputTitle">상품명</p>
           <div className="nameInput">
             <input type="text" id="productName" name="productName" value={form.productName} onChange={(e) => {
-              textNumCheck(e);
+              textNumCheck(e, 0);
               handleChange(e);
             }} maxLength='40' placeholder="상품명을 입력해 주세요." />
             <span>{textNum}/40</span>
             <span className="notice">
-              {show && <><i className="xi-ban"></i>상품명을 입력해 주세요.</>}
+              {notice[0] && <><i className="xi-ban"></i>상품명을 입력해 주세요.</>}
             </span>
           </div>
         </div>
@@ -149,7 +184,7 @@ export default function ProductRegister() {
           <p className="inputTitle">교환</p>
           <div className="formRadio">
             <label for="disable" >
-              <input id="disable" type="radio" name="change" />불가
+              <input id="disable" type="radio" name="change" checked />불가
             </label>
             <label for="ablr" >
               <input id="ablr" type="radio" name="change" />가능
@@ -159,17 +194,62 @@ export default function ProductRegister() {
 
         <div className="inputContainer">
           <p className="inputTitle">가격</p>
-          <div className="priceInput">
-            <input type="text" id="price" name="price" value={form.price} onChange={(e) => {
-              handleChange(e);
-              priceNumCheck(e);
-            }} maxLength='11' placeholder="가격을 입력해 주세요." />
-            <span className="notice">
-              {priceNotice && <><i className="xi-ban"></i>100원 이상 입력해주세요.</>}
-            </span>
+          <div >
+            <p className="priceInput">
+              <input type="text" id="price" name="price" value={form.price} onChange={(e) => {
+                priceNumCheck(e, 1);
+              }} maxLength='11' placeholder="가격을 입력해 주세요." />
+              <span className="notice">
+                {notice[1] && <><i className="xi-ban"></i>100원 이상 입력해주세요.</>}
+              </span>
+            </p>
+            <div className="formRadio">
+              <label for="include" >
+                <input id="include" type="radio" name="deliver" checked={!deliverPrice} onChange={() => setDeliverPrice(false)} />배송비포함
+              </label>
+              <label for="notInclude" >
+                <input id="notInclude" type="radio" name="deliver" onChange={() => setDeliverPrice(true)} />배송비별도
+              </label>
+            </div>
           </div>
 
         </div>
+
+        {deliverPrice &&
+
+          <div className="inputContainer">
+            <p className="inputTitle">배송비</p>
+            <div >
+              <p className="priceInput">
+                <input type="text" id="dleliverPrice" name="dleliverPrice" onChange={(e) => {
+                  deliverPriceCheck(e, 2);
+                }} maxLength='11' placeholder="배송비를 입력해 주세요." />
+                <span className="notice">
+                  {notice[2] && <><i className="xi-ban"></i>배송비는 100원부터 100,000원까지 입력할 수 있어요</>}
+
+                </span>
+                <b className="marginT">- 입력 시 구매자가 배송비까지 한 번에 결제할 수 있어요.</b>
+                <b>- 편의점택배 최소 금액 3,200원 (동일권, 350g 이하 기준)</b>
+              </p>
+
+            </div>
+
+          </div>
+        }
+        <div className="inputContainer">
+          <p className="inputTitle">설명</p>
+          <div >
+            <textarea rows="6" onChange={(e) => {
+              textAreaCheck(e, 3)
+            }}></textarea>
+            <span className="notice">
+              {notice[3] && <><i className="xi-ban"></i>상품 설명을 10글자 이상 입력해주세요.</>}
+            </span>
+          </div>
+
+
+        </div>
+
 
 
 
